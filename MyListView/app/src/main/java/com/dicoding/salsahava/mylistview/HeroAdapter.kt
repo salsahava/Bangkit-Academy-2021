cedc526088e7d0cbb312ddb98ff2db9c5c456a79
salsahava <1shavaqabitta@gmail.com>
2021-03-17 11:32:53 +0700

    Library di Android - Belajar Fundamental Aplikasi Android

--- MyListView/app/src/main/java/com/dicoding/salsahava/mylistview/HeroAdapter.kt
+++ MyListView/app/src/main/java/com/dicoding/salsahava/mylistview/HeroAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import de.hdodenhof.circleimageview.CircleImageView
 
 class HeroAdapter internal constructor(private val context: Context) : BaseAdapter() {
     internal var heroes = arrayListOf<Hero>()
@@ -32,9 +33,9 @@ class HeroAdapter internal constructor(private val context: Context) : BaseAdapt
     private inner class ViewHolder internal constructor(view: View) {
         private val txtName: TextView = view.findViewById(R.id.txt_name)
         private var txtDescription: TextView = view.findViewById(R.id.txt_description)
-        private val imgPhoto: ImageView = view.findViewById(R.id.img_photo)
+        private val imgPhoto: CircleImageView = view.findViewById(R.id.img_photo)
 
-        internal fun bind(hero: Hero) {
+        fun bind(hero: Hero) {
             txtName.text = hero.name
             txtDescription.text = hero.description
             imgPhoto.setImageResource(hero.photo)
