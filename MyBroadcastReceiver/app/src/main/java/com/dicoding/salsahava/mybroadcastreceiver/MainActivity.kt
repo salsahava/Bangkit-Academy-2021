b8d9079235846d14e0e67746472af3efef4514a4
salsahava <1shavaqabitta@gmail.com>
2021-04-01 11:39:04 +0700

    Broadcast Receiver (Broadcast dari Sebuah Event) - Belajar Fundamental Aplikasi Android

--- MyBroadcastReceiver/app/src/main/java/com/dicoding/salsahava/mybroadcastreceiver/MainActivity.kt
+++ MyBroadcastReceiver/app/src/main/java/com/dicoding/salsahava/mybroadcastreceiver/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.dicoding.salsahava.mybroadcastreceiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.dicoding.salsahava.mybroadcastreceiver.databinding.ActivityMainBinding
@@ -10,9 +15,11 @@ import com.dicoding.salsahava.mybroadcastreceiver.databinding.ActivityMainBindin
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object {
+        const val ACTION_DOWNLOAD_STATUS = "download_status"
         private const val SMS_REQUEST_CODE = 101
     }
 
+    private lateinit var downloadReceiver: BroadcastReceiver
     private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +29,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding?.root)
 
         binding?.btnPermission?.setOnClickListener(this)
+        binding?.btnDownload?.setOnClickListener(this)
+
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.d(DownloadService.TAG, "Download Selesai")
+                Toast.makeText(context, "Download Selesai", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+        registerReceiver(downloadReceiver, downloadIntentFilter)
     }
 
     override fun onClick(v: View?) {
-        when {
-            v?.id == R.id.btn_permission -> PermissionManager.check(this, android.Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE)
+        when (v?.id) {
+            R.id.btn_permission -> PermissionManager.check(
+                this,
+                android.Manifest.permission.RECEIVE_SMS,
+                SMS_REQUEST_CODE
+            )
+            R.id.btn_download -> {
+                val downloadServiceIntent = Intent(this, DownloadService::class.java)
+                startService(downloadServiceIntent)
+            }
         }
     }
 
@@ -37,8 +62,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     ) {
         if (requestCode == SMS_REQUEST_CODE) {
             when {
-                grantResults[0] == PackageManager.PERMISSION_GRANTED -> Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
-                else -> Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+                grantResults[0] == PackageManager.PERMISSION_GRANTED -> Toast.makeText(
+                    this,
+                    "Sms receiver permission diterima",
+                    Toast.LENGTH_SHORT
+                ).show()
+                else -> Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
     }
@@ -46,6 +76,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onDestroy() {
         super.onDestroy()
 
+        unregisterReceiver(downloadReceiver)
         binding = null
     }
 }
