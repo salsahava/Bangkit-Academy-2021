c5f048c8993139021e0dbc1ea427bf4774f8a7f0
salsahava <1shavaqabitta@gmail.com>
2021-04-21 12:38:21 +0700

    SoundPool dan MediaPlayer (Aplikasi MediaPlayer)

---
+++ Module5-ExtendedApplication/MyMediaPlayer/app/src/main/java/com/dicoding/salsahava/mymediaplayer/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.dicoding.salsahava.mymediaplayer
+
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    private var mMediaPlayer: MediaPlayer? = null
+    private var isReady: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnPlay = findViewById<Button>(R.id.btn_play)
+        val btnStop = findViewById<Button>(R.id.btn_stop)
+
+        btnPlay.setOnClickListener{
+            if (!isReady) mMediaPlayer?.prepareAsync()
+            else {
+                if (mMediaPlayer?.isPlaying() as Boolean) mMediaPlayer?.pause()
+                else mMediaPlayer?.start()
+            }
+        }
+
+        btnStop.setOnClickListener {
+            if (mMediaPlayer?.isPlaying() as Boolean || isReady) {
+                mMediaPlayer?.stop()
+                isReady = false
+            }
+        }
+
+        init()
+    }
+
+    private fun init() {
+        mMediaPlayer = MediaPlayer()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val attribute = AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build()
+
+            mMediaPlayer?.setAudioAttributes(attribute)
+        } else mMediaPlayer?.setAudioStreamType(AudioManager.STREAM_MUSIC)
+
+        val afd = applicationContext.resources.openRawResourceFd(R.raw.guitar_background)
+
+        try {
+            mMediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        mMediaPlayer?.setOnPreparedListener({
+            isReady = true
+            mMediaPlayer?.start()
+        })
+
+        mMediaPlayer?.setOnErrorListener({ mp, what, extra -> false })
+    }
+}
